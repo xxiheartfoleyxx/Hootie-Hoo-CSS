aecab5542e5bdfe493b72f6172f5468e9400ee0b
Beatriz <bzbustamante@gmail.com>
2015-11-25 20:28:33 -0500

    adding locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :locations
+
   devise_for :users, :path_prefix => 'd', :controllers => {:registrations => "users", :omniauth_callbacks => "omniauth_callbacks"}
 
   resources :profiles
