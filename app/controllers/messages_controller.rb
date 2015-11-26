70a6e311b99ab2f6bd8d981bd261da403b4c2d08
Beatriz <bzbustamante@gmail.com>
2015-11-26 01:33:07 -0500

    removing changes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,17 +2,13 @@ class MessagesController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-      @conversation = Conversation.find(params[:conversation_id])
-      @message = @conversation.messages.build(message_params)
-      @message.user_id = current_user.id
-      @message.save!
-      @path = conversation_path(@conversation)
-      if @message.message_attachment_id
-        respond_to do |format|
-          format.html { redirect_to :back }
-          # I've tried w/ and with no format.js as well.
-          #format.js { render: 'create.js.erb }
-        end
+    @conversation = Conversation.find(params[:conversation_id])
+    @message = @conversation.messages.build(message_params)
+    @message.user_id = current_user.id
+    @message.save!
+
+    @path = conversation_path(@conversation)
+  end
 
   private
 
